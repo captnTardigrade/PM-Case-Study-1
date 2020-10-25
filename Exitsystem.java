d441bc2c3ca792e6420b742582e890cfedbf900b
RithikaSapparapu <70932495+RithikaSapparapu@users.noreply.github.com>
2020-10-25 13:52:49 +0530

    used regex for validation of pin and card number

--- Exitsystem.java
+++ Exitsystem.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.regex.*; 
 
 class ExitSystem{
   int bill;
@@ -15,117 +16,44 @@ class ExitSystem{
    
   
   //card validation:
-  public static int size(long ccno)
-	{
-		long temp=ccno;
-		int sum=0;
-		while(temp!=0)
-		{
-			temp/=10;
-			sum++;
-		}
-	    return sum;
-	}
-	public static long[] arrayconverter(long ccno)
-	{
-		int n=size(ccno), k=n-1;;
-		long a[]=new long[n];
-		for(int i=0;i<n;i++) 
-		{
-			a[k--]=ccno%10;
-			ccno/=10;
-	    }
-		return a;
-	}
-	public static boolean match(long ccno)
-	{
-		long a[]=arrayconverter(ccno);
-		return(a[0]*10+a[1]==37||a[0]==4||a[0]==5||a[0]==6);
-	}
-	public static int oddsum(long ccno)
-	{
-		int sum=0;
-		long a[]=arrayconverter(ccno);
-		for(int i=size(ccno)-1;i>=0;i-=2)
-		{
-			sum+=a[i];
-		}
-		return sum;
-	}
-	public static long doublenumber(long a)
-	{
-		if(a<=9)
-			return a;
-		else
-			return a/10+a%10;
-	}
-	public static int evendoublesum(long ccno)
-	{
-		int sum=0;
-		long a[]=arrayconverter(ccno);
-		for(int i=size(ccno)-2;i>=0;i-=2)
-		{
-			
-			sum+=doublenumber(2*a[i]);
-		}
-		return sum;
-	}
-	public static boolean IscardValid(long ccno)
-	{
-		return (size(ccno) >= 13 && size(ccno) <= 16 )&& match(ccno)
-				&& ((evendoublesum(ccno) + oddsum(ccno)) % 10 == 0);
-	}
-  
+public static boolean IscardValid(String str) 
+{ 
+  String regex = "^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})$"; 
+  Pattern p = Pattern.compile(regex); 
+ 
+  if (str == null) { 
+    return false; 
+    }  
+  Matcher m = p.matcher(str); 
+    return m.matches(); 
+}
+	
   
   
   //pin validation:
-  public static int sizeof(int pin)
-	{
-		int temp=pin;
-		int sum=0;
-		while(temp!=0)
-		{
-			temp/=10;
-			sum++;
-		}
-	    return sum;
-	}
-	public static int[] arrayconverter1(int pin)
-	{
-		int n=size(pin), k=n-1;;
-		int arr[]=new int[n];
-		for(int i=0;i<n;i++) 
-		{
-			arr[k--]=pin%10;
-			pin/=10;
-	    }
-		return arr;
-	}
-	public static boolean startdigit(int pin)
-	{
-		int a[]=arrayconverter1(pin);
-		return(a[0]!=0);
-	}
+public static boolean IsPinValid(String str1) 
+    { 
+        String regex = "^[1-9][0-9]{5}$";
+        Pattern p1 = Pattern.compile(regex); 
+ 
+        if (str == null) { 
+            return false; 
+        }  
+  
+        Matcher m1 = p1.matcher(str1); 
+        return m1.matches(); 
+    } 
 	
-	public static boolean IsPinValid(int pin)
-	{
-		return(sizeof(pin)==6&&startdigit(pin));
-	}
-  
-  
-  
   
   //database ...?
   
-  
-  
-
+	
   // payment done by using a card
   void cardPayment(){
         Scanner sc= new Scanner(System.in);
     System.out.println("please enter your card number:");
     while(1){   
-       int cardNumber=  sc.nextInt();
+	String cardNumber= sc.nextLine();
         
        if(cardIsValid(cardNumber)){
          break;
@@ -139,7 +67,7 @@ class ExitSystem{
     
     System.out.println("please enter pin:");
     while(1){   
-       int PinNumber=  sc.nextInt();
+	String PinNumber= sc.nextLine();
         
        if(IsPinValid(PinNumber)){
          break;
